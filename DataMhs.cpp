e5ecc28bbd2be83611e5ae2135444e7d779482d5
fissilmisarmada <fissilmisarmada00@gmail.com>
2024-12-27 17:08:29 +0800

    U:Menambah Fungsi Menu Masukkan Data Mhs

--- DataMhs.cpp
+++ DataMhs.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include <conio.h>
+#include <cstdlib>  // Untuk system("cls")
+#include <conio.h>  // Untuk getch()
 using namespace std;
 
-void dMenu(){
+// Struktur Data Mahasiswa
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string tanggalLahir; // Format DD/MM/YYYY
+    float ipk;
+};
+
+// Data mahasiswa
+Mahasiswa dataMahasiswa[30];
+int pos = -1; // Menyimpan posisi terakhir data mahasiswa
+// Fungsi untuk mencari data mahasiswa berdasarkan NIM
+int cariDataByNIM(const string& nim) {
+    for (int i = 0; i <= pos; i++) {
+        if (dataMahasiswa[i].nim == nim) {
+            return i; // Data ditemukan
+        }
+    }
+    return -1; // Data tidak ditemukan
+}
+void tampilkanMenu(){  
 system("cls");
 cout << "\n=== Menu Aplikasi Data Mahasiswa ===\n";
 cout << "1. Masukkan Data Mahasiswa\n";
@@ -14,33 +37,47 @@ cout << "5. Exit\n";
 cout << "Masukkan pilihan: ";         
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+void tambahData() {
+    system("cls");  // Clear screen sebelum input data
+    if (pos >= 29) {
+        cout << "\nData mahasiswa penuh! Tidak bisa menambah data baru.\n";
+        return;
+    }
+    pos++;
+    cout << "\nMasukkan Data Mahasiswa\n";
+    cout << "Masukkan NIM: ";
+    getline(cin, dataMahasiswa[pos].nim); 
+    cout << "Masukkan Nama: ";
+    getline(cin, dataMahasiswa[pos].nama); 
+    cout << "Masukkan Alamat: ";
+    getline(cin, dataMahasiswa[pos].alamat); 
+    cout << "Masukkan Tanggal Lahir (DD/MM/YYYY): ";
+    getline(cin, dataMahasiswa[pos].tanggalLahir); 
+    cout << "Masukkan IPK: ";
+    cin >> dataMahasiswa[pos].ipk;
 }
+
 int main() {
 char pl;
 do
 {
-    dMenu();
+    tampilkanMenu();
     pl=getch();
   switch (pl)
   {
    case '1':
-    /* code */
-    mPertama("pertama");
+       tambahData();
     break;
    case '2':
-    mPertama("ke- dua");
+ 
     /* code */ 
     break;  
    case '3':
-    mPertama("ke- tiga");
+
     /* code */
     break;  
    case '4':
-    mPertama("ke- empat");
+
     /* code */
     break;  
   case '5':
